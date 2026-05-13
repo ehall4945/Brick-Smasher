c4aaad702f21da72b6c265f2810fb263a12abf4b
Eric Hall <erichall@uwm.edu>
2026-05-13 14:17:23 -0500

    Added admin functionality to site. You can now access admin panel.

--- BrickSmasher/urls.py
+++ BrickSmasher/urls.py
@@ -1,14 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 
-# App routes for pages and AJAX endpoints
 urlpatterns = [
+    path("admin/", admin.site.urls),
+
     path("", views.home, name="home"),
     path("account/", views.account_page, name="account"),
     path("movie/", views.movie_page, name="movie"),
     path("rent/", views.rent_page, name="rent"),
+
     path("dbUser/", views.db_user, name="db_user"),
     path("dbMovie/", views.db_movie, name="db_movie"),
     path("dbRent/", views.db_rent, name="db_rent"),
